a30be881a8d3ce962a75b660669b9fd3ebfba104
mukesh <mksingh199423@gmail.com>
2021-05-12 23:19:26 +0530

    main

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+credentials = "${file("verdant-art-307109-e2b1e801b21f.json")}"
+  project = "verdant-art-307109"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  zone = "us-central1-a"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+recource 
